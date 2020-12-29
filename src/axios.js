dfeae1689c8b019eb67f05baeaa1c2467c0bd5fb
Joshua-Salvador <424joshuae.b.s@gmail.com>
2020-12-29 17:16:18 +0800

    Changed baseUrl to have 'https'

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const instance = Axios.create({
   baseURL:
-    "http://chikapp-env.eba-ni56ywqs.ap-southeast-1.elasticbeanstalk.com/",
+    "https://chikapp-env.eba-ni56ywqs.ap-southeast-1.elasticbeanstalk.com/",
 });
 
 export default instance;
