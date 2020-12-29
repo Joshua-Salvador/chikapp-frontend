013da38f6f0d811db8b0143182fee0d05991a2ba
Joshua-Salvador <424joshuae.b.s@gmail.com>
2020-12-29 17:47:57 +0800

    Removed https

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const instance = Axios.create({
   baseURL:
-    "https://chikapp-env.eba-ni56ywqs.ap-southeast-1.elasticbeanstalk.com/",
+    "http://chikapp-env.eba-ni56ywqs.ap-southeast-1.elasticbeanstalk.com/",
 });
 
 export default instance;
